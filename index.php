d18033dfd17aefb87a98c99c1c6ee948bbe2fbb4
Clésar Sapelli Junior <clesarsapellijunior@gmail.com>
2021-05-16 20:41:40 -0300

    commit

--- index.php
+++ index.php
@@ -4,7 +4,6 @@
     <meta charset="UTF-8">
     <title>Jogo dos dados</title>
 </head>
-<script>
     <main id="container">
 		<div class="wrapper">
             <form action="" method="post">
@@ -20,16 +19,12 @@
 
         <div class="pessoa">
             <?php 
-                gerarFaces($pessoaDado1);
-                gerarFaces($pessoaDado2);
                 echo "<br/>";
                 echo "<h2 style=\"color: black;\">Soma das suas faces: $somaPessoa</h2>"
             ?>   
         </div>  
         <div class="maquina">
             <?php 
-                gerarFaces($maquinaDado1);
-                gerarFaces($maquinaDado2);
                 echo "<br/>";
                 echo "<h2 style=\"color: black;\">Soma das faces da máquina: $somaMaquina</h2>"
             ?>
@@ -42,7 +37,6 @@
         </div>
         </div>
     </main>
-</script>
 
 
 <style>
